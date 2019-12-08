a25a6410a3ac07a4c14dae51e240db541f2a5cef
rotem vertman <rotem.ver@gmail.com>
2019-12-08 13:58:59 +0200

    update categories

--- WWUDo.py
+++ WWUDo.py
@@ -2,12 +2,14 @@ import pandas as pd
 excel_file='DB.xlsx'
 data = pd.read_excel(excel_file)
 
+print (data)
+
 def choose_category():
     print(':בחר קטגוריה למשחק')
     print('1- בית ספר')
     print('2- בית')
     print('3- מקום ציבורי')
     choice=input()
-    data['category']
+    game(choice)
     
 choose_category()
