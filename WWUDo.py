d40737e281ab54fb0fbd38720fd95eafb6dfa4d2
Oren Shaya <58634407+OrenShaya@users.noreply.github.com>
2019-12-08 16:53:10 +0200

    Instruction added
    added instruction text file and instruction function to print it out

--- WWUDo.py
+++ WWUDo.py
@@ -1,29 +1,11 @@
 import pandas as pd
-excel_file='DB.xlsx'
-data = pd.read_excel(excel_file)
+#myData=pd.read_excel("Question_db.xlsx",encoding = "ISO-8859-1")
 
-print (data)
+def instructions():
+    file=open("instruction1.txt",'r')
+    print(file.read())
 
-def menu_player():
-    print ('בחר אפשרות')
-    print ('1- בחירת קטגוריה והתחלת משחק /n 2- צפייה בהוראות המשחק /n 3- צפייה בציונים קודמים /n 4- יציאה מהמערכת')
-    choice=int(input())
-    
-    switch (choice) {
-            case 1: choose_category();
-            case 2: 
-            case 3:
-            case 4:
-            }
-        
-
-
-def choose_category():
-    print(':בחר קטגוריה למשחק')
-    print('1- בית ספר')
-    print('2- בית')
-    print('3- מקום ציבורי')
-    choice=input()
-    game(choice)
-    
-choose_category()
+print("Running instructions...")
+instructions()
+print()
+print("Run has ended")
