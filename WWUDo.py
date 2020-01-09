49fdf885154f211762709526b9417dabdd1a8ab6
Oren Shaya <58634407+OrenShaya@users.noreply.github.com>
2020-01-09 17:10:10 +0200

    Added View_All function
    function to print all the user from requested type

--- WWUDo.py
+++ WWUDo.py
@@ -209,7 +209,17 @@ def Print_Last_Mistake(id):
                     print()
                 break
             i+=1
-    
+
+def View_All(user_type):
+    """View all the user type"""
+    #Need to change to the right file path
+    UsersDB = pd.read_excel('C:\\Users\\xxore\\Documents\\Project\\Users_db.xlsx')
+    index = 0
+    for user in UsersDB['ID']:
+        types = UsersDB['User type'][index]
+        if (types == user_type):
+            print(user)
+        index = index + 1
 
 def instructions():
     file = open("instruction1.txt", 'r')
@@ -314,12 +324,3 @@ def professional_menu():
 
 
 # --------------------------------------------------
-
-def choose_category():
-    print(':בחר קטגוריה למשחק')
-    print('1- בית ספר')
-    print('2- בית')
-    print('3- מקום ציבורי')
-    choice = input()
-    game(choice)  # TODO: game function
-
